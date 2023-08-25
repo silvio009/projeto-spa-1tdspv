4f3be3d463ba111da6521fe0d2394ca8044ac219
silvio009 <rm550821@fiap.com.br>
2023-08-24 22:49:12 -0300

    criando a modularização

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-
+import reactLogo from "./assets/react.svg"
+import Cabecalho from "./components/Cabecalho"
 export default function App() {
-
+  let reactlogoTextoalt = "logo react"
   // area declarativa
   return (
     <>
       {/*area imperativa*/}
      <div>
-      <h1>Vite +React</h1>
-      <h2>conseguimos</h2>
-      <p>olá mundo!!</p>
+      {/*um cabeçalho com um header, um h1 , uma lista ul , com 3 links*/}
+       <Cabecalho/>
+      {/*uma seção com uma div com 3 paragrafos, com 3  linhas de lorem, uma img */}
+      <section>
+        <div>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint illum ab fugiat pariatur nisi nam magnam consequatur voluptate corporis, excepturi aspernatur nesciunt. Molestias quia quae earum quo magni iste tempore.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint illum ab fugiat pariatur nisi nam magnam consequatur voluptate corporis, excepturi aspernatur nesciunt. Molestias quia quae earum quo magni iste tempore.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint illum ab fugiat pariatur nisi nam magnam consequatur voluptate corporis, excepturi aspernatur nesciunt. Molestias quia quae earum quo magni iste tempore.</p>
+         <img src={reactLogo} alt={reactlogoTextoalt} />
+        </div>
+      </section>
+      {/*um rodape com uma div , uma lista ul com3 itens e links para redes sociais  */}
+      <footer>
+        <div>
+          <ul>
+            <li><a href="#"></a>github</li>
+            <li><a href="#"></a>x</li>
+            <li><a href="#"></a>insta</li>
+          </ul>
+        </div>
+      </footer>
      </div>
       
     </>
---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,15 @@
+
+export default function Cabecalho(){
+  return(
+    <>
+        <header>
+        <h1> vite + react / colded by - rm550821</h1>
+        <ul>
+          <li><a href="#">item 1 </a></li>
+          <li><a href="#">item 2 </a></li>
+          <li><a href="#">item 3</a></li>
+        </ul>
+       </header>
+    </>
+  )
+}
