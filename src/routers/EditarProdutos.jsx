8d5ab65bae754edfbe7757460f6feef9c7f65bab
silvio009 <rm550821@fiap.com.br>
2023-09-05 22:57:16 -0300

    commit

--- src/routers/EditarProdutos.jsx
+++ src/routers/EditarProdutos.jsx
@@ -1,7 +1,50 @@
+import { useParams } from "react-router-dom"
+import { ListaProdutos } from "../components/ListaProdutos";
+import { useState } from "react";
+
 export default function EditarProdutos() {
+
+const {id} = useParams();
+
+
+
+
+  //
+document.title = "editar produtos " + id;
+
+ const ProdutoRetornadoDofiltro =ListaProdutos.filter(produto => produto.id == id);
+
+ const [produto, setProduto] = useState("nome");
+
     return (
       <div>
           <h1>EditarProdutos</h1>
+          <p>objeto selecionado : {ProdutoRetornadoDofiltro[0].nome}</p>
+          <p>valor do produto = {produto}</p>
+          <button onClick={()=>setProduto("sla")}>mudar state</button>
+          <div>
+            <form>
+              <fieldset>
+                <legend>produto selecionado</legend>
+                <div>
+                  <label htmlFor="">nome do produto</label>
+                  <input type="text" name="nome" id="idprod" defaultValue={ProdutoRetornadoDofiltro[0].nome}/>
+                  </div>
+                  <div>
+                  <label htmlFor="">descriçao</label>
+                  <input type="text" name="desc" id="iddesc" defaultValue={ProdutoRetornadoDofiltro[0].desc}/>
+                  </div>
+                  <div>
+                  <label htmlFor="">preco</label>
+                  <input type="text" name="preco" id="idpreco" defaultValue={ProdutoRetornadoDofiltro[0].preco}/>
+                  </div>
+              <div>
+                <button>editar</button>
+              </div>
+
+              </fieldset>
+            </form>
+          </div>
       </div>
     )
   }
