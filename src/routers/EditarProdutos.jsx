ae047726577c4313d3c609343ea65245ac1ead0f
silvio009 <rm550821@fiap.com.br>
2023-09-14 16:22:11 -0300

    commit antes da aula

--- src/routers/EditarProdutos.jsx
+++ src/routers/EditarProdutos.jsx
@@ -1,86 +1,89 @@
-import { useNavigate, useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom";
 import { ListaProdutos } from "../components/ListaProdutos";
 import { useState } from "react";
 
 export default function EditarProdutos() {
+  //Utilizar o HOOK useParams() para recuperar o ID passado no path
+  const { id } = useParams();
 
-const {id} = useParams();
+  document.title = "EDITAR PRODUTOS " + id;
 
+  const navigate = useNavigate();
 
+  const produtoRetornadoDoFiltro = ListaProdutos.filter(
+    (produto) => produto.id == id
+  )[0];
 
-
-  //
-document.title = "editar produtos " + id;
-
-
-const navigate =useNavigate
-
- const ProdutoRetornadoDofiltro =ListaProdutos.filter(produto => produto.id == id[0]);
-
-
-
- const [produto, setProduto] = useState({
-id:ProdutoRetornadoDofiltro.id,
-nome: ProdutoRetornadoDofiltro.nome,
-desc: ProdutoRetornadoDofiltro.desc,
-preco:ProdutoRetornadoDofiltro.preco
-});
-
-
-
-
-const handChange = (evento)=>{
-  const{name,value} = evento.target;
-  
-  setProduto({...produto,[name]:value})
-}
-
-const handSubmit = (evento) =>{
-  evento.preventDefaul();
-
-  let indice;
-
-  ListaProdutos.forEach((item,index)=>{
-    if(item.id == id)
-    indice = index;
+  //useState()
+  const [produto, setProduto] = useState({
+    id: produtoRetornadoDoFiltro.id,
+    nome: produtoRetornadoDoFiltro.nome,
+    desc: produtoRetornadoDoFiltro.desc,
+    preco: produtoRetornadoDoFiltro.preco,
   });
-  ListaProdutos.splice(indice,1,produto);
-  navigate("./Produtos.jsx")
-}
 
+  const handleChange = (event) =>{
 
-    return (
-      <div>
-          <h1>EditarProdutos</h1>
-          <p>objeto selecionado : {ProdutoRetornadoDofiltro[0].nome}</p>
-          <p>valor do produto = {produto}</p>
-          <button onClick={()=>setProduto("sla")}>mudar state</button>
-          <div>
-            <form onSubmit={handSubmit}>
-              <fieldset>
-                <legend>produto selecionado</legend>
-                <input type="hidden" name="id" onChange={handChange} value={produto.id} />
-                <div>
-                  <label htmlFor="">nome do produto</label>
-                  <input type="text" name="nome" id="idprod"  onChange={handChange} Value={produto.nome}/>
-                  </div>
-                  <div>
-                  <label htmlFor="">descriçao</label>
-                  <input type="text" name="desc" id="iddesc"  onChange={handChange} Value={produto.desc}/>
-                  </div>
-                  <div>
-                  <label htmlFor="">preco</label>
-                  <input type="text" name="preco" id="idpreco"  onChange={handChange} Value={produto.preco}/>
-                  </div>
-              <div>
-                <button>editar</button>
-              </div>
+    //Destructuring
+    const {name, value} = event.target;
 
-              </fieldset>
-            </form>
-          </div>
-
-      </div>
-    )
-  }
+    setProduto({...produto,[name]:value});
   
+  }
+
+  const handleSubmit = (event) =>{
+     event.preventDefault();
+
+     let indice;
+
+     ListaProdutos.forEach((item,index)=>{
+        if(item.id == id){
+          indice = index;
+        }
+     });
+     ListaProdutos.splice(indice,1,produto);
+     navigate("/produtos");
+  }
+
+
+  return (
+    <div>
+      <h1>EditarProdutos</h1>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <fieldset>
+            <legend>Produto Selecionado</legend>
+            <input type="hidden" name="id" value={produto.id} />
+            <div>
+              <label htmlFor="idProd">Nome do Produto</label>
+              <input type="text" name="nome" id="idProd" onChange={handleChange} value={produto.nome} />
+            </div>
+            <div>
+              <label htmlFor="idDesc">Descrição</label>
+              <input type="text" name="desc" id="idDesc" onChange={handleChange} value={produto.desc} />
+            </div>
+            <div>
+              <label htmlFor="idPreco">Preço</label>
+              <input
+                type="text"
+                name="preco"
+                id="idPreco"
+                onChange={handleChange}
+                value={produto.preco}
+              />
+            </div>
+            <div>
+              <button>EDITAR</button>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+
+        <div>
+          <p>Nome : {produto.nome}</p>
+          <p>Desc : {produto.desc}</p>
+          <p>Preço : {produto.preco}</p>
+        </div>
+    </div>
+  );
+}
--- src/routers/Produtos.jsx
+++ src/routers/Produtos.jsx
@@ -2,40 +2,51 @@ import { ListaProdutos } from "../components/ListaProdutos";
 import { Link } from "react-router-dom";
 import {AiFillEdit as Editar, AiOutlineDelete as Excluir} from "react-icons/ai";
 import classes from "./Produtos.module.css";
+import { useEffect, useState } from "react";
 
 export default function Produtos() {
 
     document.title = "Lista de Produtos";
+
+    useEffect(()=>{
+      console.log("Use-Effect que será sempre renderizado!");
+    });
+
+    const [exemplo, setExemplo] = useState(0);
 
     return (
       <div>
           <h1>LISTA DE PRODUTOS</h1>
 
         <div>
-          <table className= {classes.tableStyle}>
+          <button onClick={()=> setExemplo(exemplo + 1)}>CLICK - {exemplo}</button>
+        </div>
+
+        <div>
+          <table className={classes.tableStyle}>
             <thead>
-              <tr className={classes.tableHeaderStyles}>
-                <th className={classes.tableHeaderStyles}>ID </th>
-                <th className={classes.tableHeaderStyles}>Nome </th>
-                <th className={classes.tableHeaderStyles}>Descrição</th>
-                <th className={classes.tableHeaderStyles}>Preço</th>
-                <th className={classes.tableHeaderStyles}>Editar/Excluir</th>
+              <tr className={classes.tableHeaderStyle}>
+                <th className={classes.tableHeaderStyle}>ID</th>
+                <th className={classes.tableHeaderStyle}>Nome</th>
+                <th className={classes.tableHeaderStyle}>Descrição</th>
+                <th className={classes.tableHeaderStyle}>Preço</th>
+                <th className={classes.tableHeaderStyle}>Editar/Excluir</th>
                 </tr>
             </thead>
             <tbody>
               {ListaProdutos.map((produto, index) => (
-                <tr key={index} className ={classes.tableLineStyles}>
-                  <td className={classes.tableDataStyles}>{produto.id}</td>
-                  <td className={classes.tableDataStyles}>{produto.nome}</td>
-                  <td className={classes.tableDataStyles}>{produto.desc}</td>
-                  <td className={classes.tableDataStyles}>{produto.preco}</td>
-                  <td className={classes.tableDataStyles}><Link to={`/editar/produtos/${produto.id}`}><Editar/></Link> | <Link to={`/excluir/produtos/${produto.id}`}><Excluir/></Link></td>
+                <tr key={index} className={classes.tableLineStyle}>
+                  <td className={classes.tableDataStyle}>{produto.id}</td>
+                  <td className={classes.tableDataStyle}>{produto.nome}</td>
+                  <td className={classes.tableDataStyle}>{produto.desc}</td>
+                  <td className={classes.tableDataStyle}>{produto.preco}</td>
+                  <td className={classes.tableDataStyle}><Link to={`/editar/produtos/${produto.id}`}><Editar/></Link> | <Link to={`/excluir/produtos/${produto.id}`}><Excluir/></Link></td>
                 </tr>
-              ))}
+              ))} 
             </tbody>
             <tfoot>
               <tr>
-                <td colSpan="4" className={classes.tableLineStyles}>Total de Produtos: {ListaProdutos.length}</td>
+                <td colSpan="4" className={classes.tableDataStyle}>Total de Produtos: {ListaProdutos.length}</td>
               </tr>
             </tfoot>
           </table>
@@ -44,3 +55,4 @@ export default function Produtos() {
       </div>
     )
   }
+  
